3d4509da7bafecc615fddc9015fce2c149be4f95
Martin Ojeda <ojedamartin13@gmail.com>
2022-10-14 20:38:15 -0300

    Commiteamos ejercicios 7, 8, y 9 de la clase de Java
    Los ejercicios son con ciclos y clase Scanner y JOptionPane

---
+++ java/Leccion2/EjercicioCiclos09/src/Ciclos09/Ejercicio09.java
@@ -0,0 +1,30 @@
+/*
+Ej 09: Pedir el dia, mes y año de una fecha e indicar si la fecha es correcta.
+Suponiendo q todos los meses son de 30 dias.
+ */
+package Ciclos09;
+
+import javax.swing.JOptionPane;
+
+
+public class Ejercicio09 {
+    public static void main(String[] args) {
+        
+        int dia = Integer.parseInt(JOptionPane.showInputDialog("Digite el dia"));
+        int mes = Integer.parseInt(JOptionPane.showInputDialog("Digite el mes"));
+        int anio = Integer.parseInt(JOptionPane.showInputDialog("Digite el año"));
+        if((dia != 0)&&  (dia <= 30)){
+            if((mes != 0 ) && (mes <=12)){
+                if((anio != 0) && (anio <= 2022)){
+                    JOptionPane.showMessageDialog(null,"La fecha ingresada es: "+dia+"/"+mes+"/"+anio);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Fecha incorrecta, año incorecto");
+                }
+            }else{
+                    JOptionPane.showMessageDialog(null,"Fecha incorrecta, mes incorecto");
+                }
+        }else{
+                    JOptionPane.showMessageDialog(null,"Fecha incorrecta, dia incorecto");
+                }
+    }
+}
